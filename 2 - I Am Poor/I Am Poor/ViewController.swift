023a4528e02bf0b8a568b24052491a7701bd80e2
veronica722 <48721322+veronica722@users.noreply.github.com>
2020-02-25 16:48:42 -0500

    Add files via upload

---
+++ 2 - I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Tong Zhao on 2/25/20.
+//  Copyright © 2020 Tong Zhao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
